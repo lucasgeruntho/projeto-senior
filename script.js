e380063afc0ba99c7dc86d866f9cc3d4c50106d3
lucasgeruntho <136639689+lucasgeruntho@users.noreply.github.com>
2024-05-24 15:05:16 -0300

    salve

--- script.js
+++ script.js
@@ -65,3 +65,39 @@ function ToggleSelection(element) {
 
 
 
+
+
+
+var dropdownVisible = false;
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("btncontent");
+  var caretDown = document.querySelector(".caret-down");
+  var caretUp = document.querySelector(".caret-up");
+  dropdownVisible = !dropdownVisible;
+  if (dropdownVisible) {
+    dropdownContent.style.display = "block";
+    caretDown.style.display = "none";
+    caretUp.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+    caretDown.style.display = "block";
+    caretUp.style.display = "none";
+  }
+}
+
+document.addEventListener('click', function(event) {
+  var isClickInsideDropdown = document.getElementById('btncontent').contains(event.target);
+  var isClickOnButton = event.target.closest('.btn-nav-people');
+
+  if (!isClickInsideDropdown && !isClickOnButton) {
+    var dropdownContent = document.getElementById('btncontent');
+    var caretDown = document.querySelector(".caret-down");
+    var caretUp = document.querySelector(".caret-up");
+    dropdownContent.style.display = 'none';
+    caretDown.style.display = "block";
+    caretUp.style.display = "none";
+    dropdownVisible = false;
+  }
+});
+
